bfe3e5bb0cd2f8def28690a562f0e51a653c6ca4
M. Nizam Setiawan <nizamsetiawan15@gmail.com>
2023-10-18 14:47:24 +0700

    update 4

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ Created on Tue Oct 17 18:54:12 2023
 import pickle
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from streamlit_option_menu import option_menu
 
 
